2454b23aebbb51c6dc62dcaf6b68be268ad67788
NoobleeGt <n.baiboun@gmail.com>
2017-10-31 19:03:02 +0100

    Exercices chapter 3 complete

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    response = """
+        <div style="text-align:right;">
+          <a href={}>About</a>
+        </div>
+        Rango says hey there partner!
+    """.format(reverse('about'))
+    return HttpResponse(response)
+
+
+def about(request):
+    response = """
+        <div style="text-align:right;">
+          <a href={}>Index</a>
+        </div>
+        Rango says here is the about page.
+    """.format(reverse('index'))
+    return HttpResponse(response)
