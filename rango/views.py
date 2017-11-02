3b2a5f2f1e493ccea1560896a1a74dbb4999fc12
NoobleeGt <n.baiboun@gmail.com>
2017-11-02 20:38:48 +0100

    Chapter 6 complete

--- rango/views.py
+++ rango/views.py
@@ -4,8 +4,10 @@ from .models import Category, Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
     context_dict = {
-        'categories': category_list
+        'categories': category_list,
+        'pages': page_list
     }
     return render(request, 'rango/index.html', context_dict)
 
